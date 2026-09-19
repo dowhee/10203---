24205d09cea816cbb52c44612653eb26d12791ec
dowhee <dowhee100829@gmail.com>
2026-09-19 11:38:30 +0900

    array, dictionary

--- 1. variable.py
+++ 1. variable.py
@@ -18,3 +18,16 @@ print(a)
 
 input_var = input("숫자를 입력하세요")
 print(input_var)
+
+# dictionary
+
+questions1 = {"no":1, "question" : "답을 구하여라" , "answer":422, "score": 5, "isMultipleChoice":False}
+
+print("questions 1의 답은" ,questions1["answer"])
+
+questions = [{"no":2, "question" : "답으로 올바른 것을 고르시오" , "answer":422, "score": 3, "isMultipleChoice":True, "example": [50, 30, 35, 20, 40]},
+             {"no":3, "question" : "답을 구하여라" , "answer":422, "score": 4, "isMultipleChoice":False},
+             {"no":4, "question" : "답을 구하여라" , "answer":422, "score": 4, "isMultipleChoice":False}]
+
+print(questions[0])
+print(questions[0]["example"][0])
