ba343dcd19439d3a2033a0d3f9ec3558d330bf22
dowhee <dowhee100829@gmail.com>
2026-09-19 11:16:00 +0900

    variable 학습

---
+++ 1. variable.py
@@ -0,0 +1,20 @@
+고윤정 = 422
+
+print(고윤정)
+
+고윤정= "고윤정"
+print(고윤정)
+
+a= 123+123
+print(a)
+
+a=[123, "hi", 523]
+print(a)
+
+고윤정=422
+김도휘=829
+a=고윤정+김도휘
+print(a)
+
+input_var = input("숫자를 입력하세요")
+print(input_var)
